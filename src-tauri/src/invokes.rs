37f115645744d686e6b18fa0bc42d10e73abcbb0
syhanjin <syhanjin666@gmail.com>
2025-11-18 19:39:55 +0800

    feat: add motor encoder configuration command

--- src-tauri/src/invokes.rs
+++ src-tauri/src/invokes.rs
@@ -141,3 +141,15 @@ pub async fn config_motor_current_pi(id_kp: f32, id_ki: f32, iq_kp: f32, iq_ki: 
         Err("Motor is not connected".to_string())
     }
 }
+
+#[tauri::command]
+pub async fn config_motor_encoder(pole_pairs: u32, encoder_direction: EncoderDirection, encoder_offset: f32, encoder_type: EncoderType, state: tauri::State<'_, AppState>) -> Result<(), String> {
+    let motor_guard = state.motor.lock().await;
+    if let Some(motor) = motor_guard.as_ref() {
+        motor.send_config_command(
+            &MotorConfigCommand::ConfigEncoder { pole_pairs, encoder_direct: encoder_direction as i8, encoder_offset, encoder_type: encoder_type.to_string() },
+        ).await.map_err(|e| e.to_string())
+    } else {
+        Err("Motor is not connected".to_string())
+    }
+}
